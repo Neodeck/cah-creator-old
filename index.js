12c8be133871bcf29f88a4181ab4b29b9b21fd99
TJ Horner <me@tjhorner.com>
2015-10-13 23:45:07 -0700

    meme

---
+++ index.js
@@ -0,0 +1,96 @@
+var express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    decks = {},
+    deckTemplate = {
+      name: "",
+      accessToken: "",
+      description: "Created with CAH Creator: cahcreator.com",
+      expansion: true,
+      blackCards: [ ],
+      whiteCards: [ ]
+    };
+
+app.use(express.static(__dirname + "/public"));
+
+function genDeckId(){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for(var i=0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+function getLatestDecks(){
+  var newDecks = {};
+  for(var i in decks){
+    var deck = decks[i];
+    newDecks[i] = deck.name;
+  }
+  return newDecks;
+}
+
+io.on('connection', function(socket){
+  socket.emit("hello");
+
+  socket.on("decks:latest", function(){
+    socket.emit("decks:latest", getLatestDecks());
+  });
+
+  socket.on("deck:name", function(name){
+    if(!socket.deck){
+      var deckId = genDeckId();
+      decks[deckId] = deckTemplate;
+      var deck = decks[deckId];
+
+      deck.accessToken = genDeckId();
+      socket.deck = deckId;
+      socket.join(deckId);
+      socket.emit("deck:id", deckId);
+      socket.emit("deck:token", deck.accessToken);
+    }else{
+      decks[socket.deck].name = name;
+      socket.to(socket.deck).emit("deck:name", name);
+    }
+  });
+
+  socket.on("deck:card:black", function(card){
+    if(socket.deck){
+      decks[socket.deck].blackCards.push({text: card, pick: 1});
+      socket.to(socket.deck).emit("deck:card:black", card);
+      socket.emit("deck:card:black", card);
+    }
+  });
+
+  socket.on("deck:card:white", function(card){
+    if(socket.deck){
+      decks[socket.deck].whiteCards.push(card);
+      socket.to(socket.deck).emit("deck:card:white", card);
+      socket.emit("deck:card:white", card);
+    }
+  });
+
+  socket.on("deck:access", function(params){
+    if(decks[params.id]){
+      var deck = decks[params.id];
+      socket.join(params.id);
+      if(deck.accessToken === params.token) socket.deck = params.id;
+      socket.emit("deck:id", params.id);
+      socket.emit("deck:editor", (deck.accessToken === params.token));
+      socket.emit("deck:name", deck.name);
+      socket.emit("deck:cards:black", deck.blackCards);
+      socket.emit("deck:cards:white", deck.whiteCards);
+    }else{
+      socket.emit("deck:access:err", "That deck doesn't exist. Note that decks are not stored permanently.");
+    }
+  });
+});
+
+app.get('/creator', function(req, res){
+  res.redirect('/#creator');
+});
+
+http.listen(process.env.PORT || 3000);
---
+++ public/js/main.js
@@ -0,0 +1,133 @@
+$(document).ready(function(){
+  var socket = io.connect();
+
+  var deckId, deckToken;
+
+  function addCard(type, card){
+    var $newCard = $("<div class='card " + type + "'></div>");
+        $newCard.text(card);
+    $(".card." + type).last().after($newCard);
+  }
+
+  function gotoDeck(id){
+    socket.emit("deck:access", {id: id, token: undefined});
+
+    console.log("Loading deck " + id);
+
+    $("body").addClass("creator-mode").removeClass("landing-mode");
+    $(".loading-overlay").fadeIn();
+  }
+
+  socket.on("hello", function(){
+    socket.emit("decks:latest");
+  });
+
+  socket.on("decks:latest", function(decks){
+    for(var i in decks){
+      var deck = decks[i],
+          $link = $("<a href='#'>" + deck + "</a><br>");
+          $link.click(function(){gotoDeck(i)});
+      $(".latest-decks").append($link);
+    }
+  });
+
+  socket.on("deck:id", function(id){
+    deckId = id;
+    $(".card-ui").slideDown();
+    $(".loading-overlay").fadeOut();
+    window.location = "#creator-" + id;
+  });
+
+  socket.on("deck:token", function(token){
+    deckToken = token;
+  });
+
+  socket.on("deck:name", function(name){
+    $(".deck-name").val(name);
+  });
+
+  socket.on("deck:access:err", function(message){
+    $("body").addClass("landing-mode").removeClass("creator-mode");
+    $(".loading-overlay").fadeOut();
+    alert(message);
+  });
+
+  socket.on("deck:card:black", function(card){
+    addCard("black", card);
+  });
+
+  socket.on("deck:card:white", function(card){
+    addCard("white", card);
+  });
+
+  socket.on("deck:cards:black", function(cards){
+    cards.forEach(function(card){
+      addCard("black", card.text);
+    });
+  });
+
+  socket.on("deck:cards:white", function(cards){
+    cards.forEach(function(card){
+      addCard("white", card);
+    });
+  });
+
+  socket.on("deck:editor", function(haveAccess){
+    if(!haveAccess){
+      $("input, textarea").prop("disabled", true);
+      $(".deck-name").before("[read-only]");
+      $(".deck-share").remove();
+    }
+  });
+
+  socket.on("deck:viewers", function(count){
+    $(".viewer-count").text("Viewers: " + count);
+  });
+
+  $("#start").click(function(){
+    $("body").addClass("creator-mode").removeClass("landing-mode");
+    window.location = "#creator";
+  });
+
+  $(".deck-share").click(function(){
+    prompt("Sharing link:", "http://" + window.location.host + "/#creator-" + deckId + "/" + deckToken);
+  });
+
+  $(".deck-export").click(function(){
+    alert("Coming soon! This will be compatible with Cards Against Equestria (and forks).");
+  });
+
+  $(".deck-name").keyup(function(){
+    socket.emit("deck:name", $(".deck-name").val());
+  });
+
+  $("#black-card-input").keypress(function(e){
+    if(e.keyCode === 13){
+      e.preventDefault();
+      socket.emit("deck:card:black", $("#black-card-input").val());
+      $("#black-card-input").val("");
+    }
+  });
+
+  $("#white-card-input").keypress(function(e){
+    if(e.keyCode === 13){
+      e.preventDefault();
+      socket.emit("deck:card:white", $("#white-card-input").val());
+      $("#white-card-input").val("");
+    }
+  });
+
+  if(window.location.hash === "#creator") $("#start").click();
+
+  if(window.location.hash.indexOf("#creator") === 0 && window.location.hash !== "#creator"){
+    var deckId = window.location.hash.split("/")[0].substr(9),
+        deckToken = window.location.hash.split("/")[1];
+
+    socket.emit("deck:access", {id: deckId, token: deckToken});
+
+    console.log("Loading deck " + deckId);
+
+    $("body").addClass("creator-mode").removeClass("landing-mode");
+    $(".loading-overlay").fadeIn();
+  }
+});
